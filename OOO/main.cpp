176d7394ccb009378098aa987bde76a3112fb5a1
xiaoxiao zheng <xiaoxiaozheng@yis013-6-res.dynamic.ucsd.edu>
2014-12-11 16:19:48 -0800

    Initial Commit

---
+++ OOO/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OOO
+//
+//  Created by xiaoxiao zheng on 12/11/14.
+//  Copyright (c) 2014 xiaoxiao zheng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
