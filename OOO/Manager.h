7f6a4dc7b263197030ec34b796805845393632e9
emmazheng <zhengxiaoxiao.mun@gmail.com>
2014-12-13 19:24:03 -0800

    constructing all the component

---
+++ OOO/Manager.h
@@ -0,0 +1,13 @@
+//
+//  Manager.h
+//  OOO
+//
+//  Created by xiaoxiao zheng on 12/13/14.
+//  Copyright (c) 2014 xiaoxiao zheng. All rights reserved.
+//
+
+#ifndef OOO_Manager_h
+#define OOO_Manager_h
+
+
+#endif
---
+++ OOO/Master.h
@@ -0,0 +1,13 @@
+//
+//  Master.h
+//  OOO
+//
+//  Created by xiaoxiao zheng on 12/13/14.
+//  Copyright (c) 2014 xiaoxiao zheng. All rights reserved.
+//
+
+#ifndef OOO_Master_h
+#define OOO_Master_h
+
+
+#endif
